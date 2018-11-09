d77bd44ba12c380009e8939cf663c32c02ed8e3f
Gangadhar S <32536686+Gangadharsmg@users.noreply.github.com>
2018-11-09 09:40:45 +0530

    Add files via upload

---
+++ relatedproduct.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+<!--/start-latest-->
+		<div class="collection-section">
+		     <h3 class="tittle">Related Products</h3>
+
+		   <div class="fashion-info">
+				<div class="col-md-4 fashion-grids">
+					<figure class="effect-bubba">
+						<img src="images/1.jpg" height="350" alt=""/>
+						<figcaption>
+							<h4>Ecomm Shop</h4>
+							<p class="cart"><a href="groceries.php">Shop</a></p>				
+						</figcaption>			
+					</figure>		
+				</div>
+				<div class="col-md-4 fashion-grids">
+					<figure class="effect-bubba">
+						<img src="images/37.jpg" alt=""/>
+						<figcaption>
+							<h4>Ecomm Shop</h4>
+								<p class="cart"><a href="personalcare.php">Shop</a></p>				
+						</figcaption>			
+					</figure>		
+				</div>
+				<div class="col-md-4 fashion-grids">
+					<figure class="effect-bubba">
+						<img src="images/24.jpg" alt=""/>
+						<figcaption>
+							<h4>Ecomm Shop</h4>
+							<p class="cart"><a href="brandeditems.php">Shop</a></p>							
+						</figcaption>			
+					</figure>		
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+       <!--//latest-->
+			</div>
+	</div>
+<!-- //products -->
+</body>
+</html>
